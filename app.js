81f9191c283382afc6fd16ed0a7241e464ba3c3a
JOHN SHAFF <JOHNSHAFF@GMAIL.COM>
2016-05-13 15:03:50 -0700

    Created multiply() declarative function and tested positive

--- app.js
+++ app.js
@@ -27,10 +27,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(){}
+function multiply(x,y) {
+  var sumResult = x * y;
+  var sumMessage = 'The product of ' + x + ' and ' + y + ' is ' + sumResult + '.';
+  return[sumResult, sumMessage];
+}
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(4,7);
+testMultiply(4,7);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
