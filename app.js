e6d6f2791d57cb91876aa1ab39961c792df48ff0
Michael Shipley Barakat <michaebkats@gmail.com>
2016-05-13 15:48:37 -0700

    uploadign problem3 branch and new changes to lab work

--- app.js
+++ app.js
@@ -48,10 +48,16 @@ Write a function called sumAndMultiply() that takes in three numbers as argument
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(){}
+function sumAndMultiply(x, y, z){
+  var sum = x + y + z;
+  var prod = x * y * z;
+  var element1 = x + ' and ' + y + ' and ' + z + ' sum to ' + sum + '.';
+  var element2 = 'The product of ' + x + ' and ' + y + ' and ' + z + ' is ' + prod + '.';
+  return[ sum, element1, element2 ];
+}
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. don't forget to create a new branch for your work on the next question!
 
